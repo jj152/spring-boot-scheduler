2de294fdfcd85b3e29a5b5d3cf9f040ed8ee8106
jj152 <ranadipdas.gcect@gmail.com>
2023-06-16 02:29:56 +0530

    initial commit

---
+++ src/test/java/com/example/springBootScheduler/SpringBootSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
